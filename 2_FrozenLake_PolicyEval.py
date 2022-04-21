e2d641d24ed380168fa0d2071f834cdd271f06e8
pabair <pabair@users.noreply.github.com>
2022-04-21 22:21:41 +0200

    solutions lab2

--- 2_FrozenLake_PolicyEval.py
+++ 2_FrozenLake_PolicyEval.py
@@ -1,23 +1,22 @@
 import gym
 import random
 
-env = gym.make("FrozenLake-v1", is_slippery=False)
+env = gym.make("FrozenLake-v1", is_slippery=True)
+discount = 1
 
 random.seed(0)
 env.seed(0)
 
-print("## Frozen Lake ##")
-print("Start state:")
+actions = range(0, env.env.nA)
+states = range(0, env.env.nS)
+model = env.env.P
+
+action2string = {0: "Left", 1: "Down", 2: "Right", 3: "Up"}
 env.render()
 
-no_of_actions = env.env.nA
-action2string = {0: "Left", 1: "Down", 2: "Right", 3: "Up"}
+# put your solution here
 
-state = env.reset()
-done = False
 
-while not done:
-    action = random.randint(0, no_of_actions-1)  # choose a random action
-    state, reward, done, _ = env.step(action)
-    print(f"\nAction:{action2string[action]}, new state:{state}, reward:{reward}")
-    env.render()
+
+
+
